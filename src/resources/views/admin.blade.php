1c7dd0718be8b0fe18cde4b4f50ada0f1e74b062
yamaguchi tomoaki <t.yamaguchi@en-gineer.com>
2024-05-14 23:48:43 +0900

    5月14日分終了

--- src/resources/views/admin.blade.php
+++ src/resources/views/admin.blade.php
@@ -117,6 +117,64 @@
                             <a class="modal__detail-button" href="#{{$contact->id}}">詳細</a>
                         </td>
                     </tr>
+
+                    <div class="modal" id="{{$contact->id}}">
+                        <a href="#!" class="modal-overlay"></a>
+                        <div class="modal__inner">
+                            <div class="modal__content">
+                                <form class="modal__detail-form" action="/delete" method="post">
+                                    @csrf
+                                    <div class="modal-form__group">
+                                        <label class="modal-form__label" for="">お名前</label>
+                                        <p>{{$contact->last_name}}{{$contact->first_name}}</p>
+                                    </div>
+
+                                    <div class="modal-form__group">
+                                        <label class="modal-form__label" for="">性別</label>
+                                        <p>
+                                            @if($contact->gender == 1)
+                                            男性
+                                            @elseif($contact->gender == 2)
+                                            女性
+                                            @else
+                                            その他
+                                            @endif
+                                        </p>
+                                    </div>
+
+                                    <div class="modal-form__group">
+                                        <label class="modal-form__label" for="">メールアドレス</label>
+                                        <p>{{$contact->email}}</p>
+                                    </div>
+
+                                    <div class="modal-form__group">
+                                        <label class="modal-form__label" for="">電話番号</label>
+                                        <p>{{$contact->tell}}</p>
+                                    </div>
+
+                                    <div class="modal-form__group">
+                                        <label class="modal-form__label" for="">住所</label>
+                                        <p>{{$contact->address}}</p>
+                                    </div>
+
+                                    <div class="modal-form__group">
+                                        <label class="modal-form__label" for="">お問い合わせの種類</label>
+                                        <p>{{$contact->category->content}}</p>
+                                    </div>
+
+                                    <div class="modal-form__group">
+                                        <label class="modal-form__label" for="">お問い合わせ内容</label>
+                                        <p>{{$contact->detail}}</p>
+                                    </div>
+                                    <input type="hidden" name="id" value="{{ $contact->id }}">
+                                    <input class="modal-form__delete-btn btn" type="submit" value="削除">
+
+                                </form>
+                            </div>
+
+                            <a href="#" class="modal__close-btn">×</a>
+                        </div>
+                    </div>
                     @endforeach
                 </table>
             </div>
